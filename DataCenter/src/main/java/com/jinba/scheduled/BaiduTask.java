c89287610df48d02bffb301d7b7624b195c552c4
duweixing <shixueyouling55@163.com>
2016-01-10 18:00:25 +0800

    百度新闻

---
+++ DataCenter/src/main/java/com/jinba/scheduled/BaiduTask.java
@@ -0,0 +1,71 @@
+package com.jinba.scheduled;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.jinba.dao.MysqlDao;
+import com.jinba.pojo.NewsEntity;
+import com.jinba.scheduled.baidu.BaiduDetailClawer;
+import com.jinba.scheduled.baidu.BaiduListClawer;
+import com.jinba.spider.core.Params;
+import com.jinba.utils.CountDownLatchUtils;
+import com.jinba.utils.LoggerUtil;
+
+@Component
+public class BaiduTask implements Runnable {
+
+	@Autowired
+	private MysqlDao dao;
+	@Value("${sgclaw.thread.pool}")
+	private int threadPoolSize = 30;
+	private ExecutorService listThreadPool;
+	
+	public void run() {
+		List<String> cityList = dao.getAreaList();
+		int listSize = cityList.size();
+		CountDownLatchUtils listCdl = new CountDownLatchUtils(listSize);
+		List<Future<List<NewsEntity>>> resList = new ArrayList<Future<List<NewsEntity>>>();
+		LoggerUtil.TaskInfoLog("[" + this.getClass().getSimpleName() + "][Start][CitySize " + cityList.size() + "]");
+		listThreadPool = Executors.newFixedThreadPool(threadPoolSize);
+		for (String eachCity : cityList) {
+			String[] cityInfo = eachCity.split("_");
+			String cityName = cityInfo[0];
+			String areaCode = cityInfo[1];
+			Map<Params, String> paramsMap = new HashMap<Params, String>();
+			paramsMap.put(Params.area, cityName);
+			paramsMap.put(Params.citycode, areaCode);
+			BaiduListClawer listClawer = new BaiduListClawer(paramsMap, listCdl);
+			resList.add(listThreadPool.submit(listClawer));
+		}
+		for (Future<List<NewsEntity>> future : resList) {
+			List<NewsEntity> detailList;
+			try {
+				detailList = future.get();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+				continue;
+			} catch (ExecutionException e) {
+				e.printStackTrace();
+				continue;
+			}
+			for (NewsEntity newsEntity : detailList) {
+				BaiduDetailClawer detail = new BaiduDetailClawer(newsEntity);
+				detail.detailAction();
+			}
+		}
+		LoggerUtil.TaskInfoLog("[" + this.getClass().getSimpleName() + "][Done]");
+		listThreadPool.shutdownNow();
+		listThreadPool = null;
+	}
+
+}
---
+++ DataCenter/src/main/java/com/jinba/scheduled/baidu/BaiduListClawer.java
@@ -0,0 +1,143 @@
+package com.jinba.scheduled.baidu;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import org.apache.catalina.util.URLEncoder;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.time.FastDateFormat;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.jinba.core.BaseListClawer;
+import com.jinba.dao.MysqlDao;
+import com.jinba.pojo.NewsEntity;
+import com.jinba.spider.core.Params;
+import com.jinba.utils.CountDownLatchUtils;
+import com.jinba.utils.MD5;
+
+public class BaiduListClawer extends BaseListClawer<NewsEntity> implements Callable<List<NewsEntity>>{
+	
+	private static final int TARGETID = 3;//百度为3
+	//##地区名称   $$替换当前第几条开始
+	private static String tempUrl = "http://m.baidu.com/news?tn=bdapinewsearch&word=##&pn=$$&rn=20&ct=1";
+	
+	private static final String FROMHOST = "m.baidu.com";
+	private static FastDateFormat sim = FastDateFormat.getInstance("yyyy-MM-dd HH:mm:ss");
+	private static FastDateFormat dateFormat = FastDateFormat.getInstance("yyyy-MM-dd");
+	private String areaName;
+	private String areaCode;
+	
+	public BaiduListClawer(Map<Params, String> paramsMap, CountDownLatchUtils cdl) {
+		super(TARGETID, cdl);
+		this.paramsMap = paramsMap;
+		areaName = paramsMap.get(Params.area);
+		areaCode = paramsMap.get(Params.citycode);
+	}
+
+	@Override
+	protected ActionRes initParams() {
+		return ActionRes.INITSUCC;
+	}
+
+	@Override
+	protected void analysisAction(List<NewsEntity> box) {
+		int pageIndex = 0;
+		String areaNameEn = new URLEncoder().encode(areaName);
+		boolean next = true;
+		do {
+			next = false;
+			String url = tempUrl.replace("##", areaNameEn).replace("$$", String.valueOf(pageIndex * 20));
+			String html = httpGet(url);
+			if (StringUtils.isBlank(html)) {
+				break;
+			}
+			JSONObject infoObject = null;
+			try {
+				infoObject = JSONObject.parseObject(html);
+				JSONArray listAll = infoObject.getJSONArray("list");
+				for(int i = 0; i < listAll.size(); i++){
+					JSONObject tempInfo = listAll.getJSONObject(i);
+					NewsEntity bean = new NewsEntity();
+					
+					//资讯来源
+					String source = tempInfo.getString("author");
+					if(!StringUtils.isBlank(source)){
+						bean.setSource(source);
+					}
+					String today = dateFormat.format(new Date());
+					//资讯时间
+					String newstimeStr = tempInfo.getString("sortTime");
+					if(!StringUtils.isBlank(newstimeStr)){
+						long newsTimeTemp = Long.parseLong(newstimeStr);
+						String newstime = sim.format(new Date(newsTimeTemp * 1000));//资讯时间
+						String newsdate = dateFormat.format(new Date(newsTimeTemp * 1000));//资讯时间的天数，木有时分秒
+						String fromKey = MD5.GetMD5Code(source + newstime);
+	//					String fromKey = MD5Encoder.encode(new String(source + newstime).getBytes());
+						//先判断数据库中是否存在这条数据，如果存在则不进行写入
+						String selectSql = "select newsid from t_news where fromhost='" + FROMHOST + "' and fromkey='" + fromKey + "'";;
+						List<Map<String, Object>> selectRes = MysqlDao.getInstance().select(selectSql);
+						if (selectRes != null && selectRes.size() > 0) {
+							continue;
+						}
+						if(today.equals(newsdate)){//先根据时间去判断是否今天的，如果没有一条是今天的，则不进行抓取
+							next = true;
+							bean.setNewstime(newstime);//资讯时间
+							bean.setPosttime(sim.format(new Date()));//发布时间为当前系统时间
+							bean.setFromkey(fromKey);//信息来源主键
+						}else{
+							continue;
+						}
+					}
+					
+					bean.setAreacode(areaCode);//地区代码
+					bean.setFromhost(FROMHOST);//信息来源
+					//资讯标题
+					String title = tempInfo.getString("title");
+					if(!StringUtils.isBlank(title)){
+						bean.setTitle(title);
+					}
+					//转自网页
+					String fromurl = tempInfo.getString("url");
+					if(!StringUtils.isBlank(fromurl)){
+						bean.setFromurl(fromurl);
+					}
+					box.add(bean);
+				}
+				pageIndex++;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} while (next && pageIndex <= 10);
+	}
+	
+	public List<NewsEntity> call() throws Exception {
+		List<NewsEntity> list = this.listAction();
+		return list;
+	}
+	
+	public static void main(String[] args) {
+		ClassPathXmlApplicationContext application = new ClassPathXmlApplicationContext(new String[]{"database.xml"});
+		application.start();
+		Map<Params, String> paramsMap = new HashMap<Params, String>();
+		paramsMap.put(Params.area, "东城区");
+		paramsMap.put(Params.citycode, "110101");
+		try {
+			List<NewsEntity> l = new BaiduListClawer(paramsMap, new CountDownLatchUtils(1)).listAction();
+			for (NewsEntity newsEntity : l) {
+				System.out.println(newsEntity);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+//		long newsTimeTemp = Long.parseLong("1450773685");
+//		String newstime = sim.format(new Date(newsTimeTemp * 1000));
+//		System.err.println(newstime);
+	}
+
+
+}
